ed1b5c98226d49a8459ca5e2364aa64ce018c6bd
cyberschooled <alex.green@itas.ca>
2025-05-01 09:11:24 -0700

    moved pages to Components and styled layout

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./assets/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout.js"
-import NoPage from "./pages/NoPage.js"
-import Users from "./pages/Users.js"
-import Schedule from "./pages/Schedule.js"
-import Home from "./pages/Home.js";
+import NoPage from "./Components/NoPage.js"
+import Users from "./Components/Users.js"
+import Schedule from "./Components/Schedule.js"
+import Home from "./Components/Home.js";
 
 function App() {
   return (
--- app/src/Components/Layout.js
+++ app/src/Components/Layout.js
@@ -5,23 +5,29 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/schedule">Schedule</Link>
-          </li>
-        </ul>
-      </nav>
+      <header className="layout-header">
+        <nav className="layout-nav">
+          {/* Logo on the left */}
+          <img
+            src="/logo192.png"
+            alt="Logo"
+            className="layout-logo"
+          />
 
-      <Outlet />
+          {/* Navigation links */}
+          <ul className="layout-nav-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/schedule">Schedule</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="layout-main">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 };
 
 export default Layout;
