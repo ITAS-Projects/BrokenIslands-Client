9a0c0b2033d31d4952e403d0e926761cec57cb57
cyberschooled <alex.green@itas.ca>
2025-05-02 11:25:12 -0700

    cleanup of unused files and links

--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,11 @@ import "./assets/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout.js"
 import NoPage from "./Components/NoPage.js"
-// import Users from "./Components/Users.js"
-// import Schedule from "./Components/Schedule.js"
 import SchedulesList from "./Components/SchedulesList.jsx";
 import Home from "./Components/Home.js";
 import EditPerson from "./Components/EditPerson.jsx";
 import EditSchedule from "./Components/EditSchedule.jsx";
 import PeopleList from "./Components/PeopleList.jsx";
-// import NewPerson from "./Components/NewPerson.js";
 import AddPerson from "./Components/AddPerson.jsx";
 import AddSchedule from "./Components/AddSchedule.jsx";
 
@@ -26,7 +23,6 @@ function App() {
           <Route exact path="schedule" element={<SchedulesList />} />
           <Route path="schedule/new" element={<AddSchedule />} />
           <Route path="schedule/edit/:id" element={<EditSchedule />} />
-
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
--- app/src/Components/Layout.js
+++ app/src/Components/Layout.js
@@ -19,7 +19,6 @@ const Layout = () => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/users">Users</Link></li>
             <li><Link to="/schedule">Schedule</Link></li>
-            <li><Link to="/data">Data</Link></li>
           </ul>
         </nav>
       </header>
